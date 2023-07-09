406aa21db9032a25c1e6505785188c8d531d5e91
Yahya Batulu <yahyabatulu00@gmail.com>
2023-07-09 16:32:34 +0300

    initial update

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# canvaspixel.net-overlay
-CanvasPixel.net Image overlay
+# Overlay Picture for canvaspixel.net
+
+<a href="https://forthebadge.com"><img src="https://forthebadge.com/images/badges/made-with-typescript.svg" height="35"></a>
+<a href="https://forthebadge.com"><img src="https://forthebadge.com/images/badges/gluten-free.svg" height="35"></a>
+<a href="https://forthebadge.com"><img src="https://forthebadge.com/images/badges/powered-by-coders-sweat.svg" height="35"></a>
+
+# Origin of the repisitory
+https://github.com/Woyken/pixelplanet.fun-OverlayPicture
+
+# How to setup
+
+1. Install tampermonkey addon for your browser from - <https://www.tampermonkey.net/> .
+2. Open <https://bypikod.github.io/canvaspixel.net-overlay/pixelPlanetOverlay-loader.user.js> .
+3. Click "Install".
+4. Enjoy.
+
+# Preview
+
+![Screenshot preview 1](assets/images/Screenshot1.png)
+![Screenshot preview 2](assets/images/Screenshot2.png)
+![Usage preview GIF](assets/images/usagePreview.gif)
---
+++ index.js
@@ -0,0 +1,107 @@
+
+// ==UserScript==
+// @name         canvaspixel.net picture overlay
+// @namespace    https://github.com/ByPikod/canvaspixel.net-overlay
+// @version      1.1.12
+// @description  Add your picture as overlay to canvaspixel.net
+// @author       Woyken
+// @include      https://canvaspixel.net/*
+// @include      https://www.canvaspixel.net/*
+// @grant        none
+// @downloadURL  https://bypikod.github.io/canvaspixel.net-overlya/pixelPlanetOverlay-loader.user.js
+// ==/UserScript==
+/**/
+
+true&&(function polyfill() {
+    const relList = document.createElement('link').relList;
+    if (relList && relList.supports && relList.supports('modulepreload')) {
+        return;
+    }
+    for (const link of document.querySelectorAll('link[rel="modulepreload"]')) {
+        processPreload(link);
+    }
+    new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type !== 'childList') {
+                continue;
+            }
+            for (const node of mutation.addedNodes) {
+                if (node.tagName === 'LINK' && node.rel === 'modulepreload')
+                    processPreload(node);
+            }
+        }
+    }).observe(document, { childList: true, subtree: true });
+    function getFetchOpts(script) {
+        const fetchOpts = {};
+        if (script.integrity)
+            fetchOpts.integrity = script.integrity;
+        if (script.referrerpolicy)
+            fetchOpts.referrerPolicy = script.referrerpolicy;
+        if (script.crossorigin === 'use-credentials')
+            fetchOpts.credentials = 'include';
+        else if (script.crossorigin === 'anonymous')
+            fetchOpts.credentials = 'omit';
+        else
+            fetchOpts.credentials = 'same-origin';
+        return fetchOpts;
+    }
+    function processPreload(link) {
+        if (link.ep)
+            // ep marker = processed
+            return;
+        link.ep = true;
+        // prepopulate the load record
+        const fetchOpts = getFetchOpts(link);
+        fetch(link.href, fetchOpts);
+    }
+}());
+
+const scriptRel = 'modulepreload';const assetsURL = function(dep) { return "/"+dep };const seen = {};const __vitePreload = function preload(baseModule, deps, importerUrl) {
+    // @ts-expect-error true will be replaced with boolean later
+    if (!true || !deps || deps.length === 0) {
+        return baseModule();
+    }
+    const links = document.getElementsByTagName('link');
+    return Promise.all(deps.map((dep) => {
+        // @ts-expect-error assetsURL is declared before preload.toString()
+        dep = assetsURL(dep);
+        if (dep in seen)
+            return;
+        seen[dep] = true;
+        const isCss = dep.endsWith('.css');
+        const cssSelector = isCss ? '[rel="stylesheet"]' : '';
+        const isBaseRelative = !!importerUrl;
+        // check if the file is already preloaded by SSR markup
+        if (isBaseRelative) {
+            // When isBaseRelative is true then we have `importerUrl` and `dep` is
+            // already converted to an absolute URL by the `assetsURL` function
+            for (let i = links.length - 1; i >= 0; i--) {
+                const link = links[i];
+                // The `links[i].href` is an absolute URL thanks to browser doing the work
+                // for us. See https://html.spec.whatwg.org/multipage/common-dom-interfaces.html#reflecting-content-attributes-in-idl-attributes:idl-domstring-5
+                if (link.href === dep && (!isCss || link.rel === 'stylesheet')) {
+                    return;
+                }
+            }
+        }
+        else if (document.querySelector(`link[href="${dep}"]${cssSelector}`)) {
+            return;
+        }
+        const link = document.createElement('link');
+        link.rel = isCss ? 'stylesheet' : scriptRel;
+        if (!isCss) {
+            link.as = 'script';
+            link.crossOrigin = '';
+        }
+        link.href = dep;
+        document.head.appendChild(link);
+        if (isCss) {
+            return new Promise((res, rej) => {
+                link.addEventListener('load', res);
+                link.addEventListener('error', () => rej(new Error(`Unable to preload CSS for ${dep}`)));
+            });
+        }
+    })).then(() => baseModule());
+};
+
+__vitePreload(() => import('./pixelPlanetOverlay-loader.user.js'),true?[]:void 0);
